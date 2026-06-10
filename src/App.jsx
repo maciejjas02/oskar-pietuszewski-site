211f48c08d47c8319537f40c5a81888f3f3410a3
Maciej Jas <maciejjastrzebski2002@gmail.com>
2026-06-10 13:05:04 +0200

    Dodanie komponentu Terminarz (Schedule) z mockowymi danymi pod przyszłe API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import PlayerProfile from './components/PlayerProfile';
+import Schedule from './components/Schedule'; 
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#09090b] font-sans selection:bg-yellow-500 selection:text-black overflow-hidden pb-10">
+    <div className="min-h-screen bg-[#09090b] font-sans selection:bg-yellow-500 selection:text-black overflow-hidden pb-16">
       <PlayerProfile />
+      <Schedule /> {}
     </div>
   );
 }
